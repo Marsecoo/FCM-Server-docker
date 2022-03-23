4624d471324737a67c5126d6a1a265cf5b15848d
Neo <c.min.tseng@gmail.com>
2022-03-23 11:51:06 +0800

    add common library : data - fcm_token

---
+++ common/fcm_token/src/main/java/tw/gov/president/cks/fcm/Constant.kt
@@ -0,0 +1,7 @@
+package tw.gov.president.cks.fcm
+
+class Constant {
+    companion object {
+        const val FCM_TOKEN = "fcm_token"
+    }
+}
---
+++ common/fcm_token/src/main/java/tw/gov/president/cks/fcm/data/FCMToken.kt
@@ -0,0 +1,6 @@
+package tw.gov.president.cks.fcm.data
+
+data class FCMToken(
+    var deviceId: String = "",
+    var fcmToken: String = ""
+ )
