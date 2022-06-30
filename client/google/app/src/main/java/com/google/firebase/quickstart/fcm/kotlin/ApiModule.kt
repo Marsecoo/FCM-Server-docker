607f8e62f6b974cd9fed0abc6830cbf1b6da727b
Neo <c.min.tseng@gmail.com>
2022-06-30 15:30:27 +0800

    modify signingConfig &  google services signing

--- client/google/app/src/main/java/com/google/firebase/quickstart/fcm/kotlin/ApiModule.kt
+++ client/google/app/src/main/java/com/google/firebase/quickstart/fcm/kotlin/ApiModule.kt
@@ -1,5 +1,6 @@
 package com.google.firebase.quickstart.fcm.kotlin
 
+import com.google.firebase.quickstart.fcm.BuildConfig
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
