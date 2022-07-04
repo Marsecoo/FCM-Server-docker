336ad7ece7f464f9fab4f7e12c14b0669ccc32dc
Neo <c.min.tseng@gmail.com>
2022-07-05 00:04:20 +0800

    sync master

--- server/Spring_Boot/app/src/main/java/com/weng/demo/fcm/controller/PushNotificationController.java
+++ server/Spring_Boot/app/src/main/java/com/weng/demo/fcm/controller/PushNotificationController.java
@@ -1,25 +1,15 @@
 package com.weng.demo.fcm.controller;
 
-import com.weng.demo.fcm.FcmInitializer;
 import com.weng.demo.fcm.model.PushNotificationRequest;
 import com.weng.demo.fcm.model.PushNotificationResponse;
+import com.weng.demo.fcm.service.FCMService;
 import com.weng.demo.fcm.service.PushNotificationService;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-
-import java.net.URI;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-
-import tw.gov.president.cks.fcm.data.FCMToken;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @author Jason Weng
@@ -27,37 +17,49 @@ import tw.gov.president.cks.fcm.data.FCMToken;
  */
 @RestController
 public class PushNotificationController {
-   private static final Logger logger = LoggerFactory.getLogger(FcmInitializer.class);
-   private final HashMap<String,String> fakeTokenDB = new HashMap<String,String>();
-   private PushNotificationService pushNotificationService;
+    private Logger Logger = LoggerFactory.getLogger(PushNotificationController.class);
 
-   public PushNotificationController(PushNotificationService pushNotificationService) {
-      this.pushNotificationService = pushNotificationService;
-   }
+    private PushNotificationService pushNotificationService;
+    private TMPDB tmdb;
 
-   @PostMapping("/registerToken")
-   public ResponseEntity  registerFCMToken(@RequestBody FCMToken request) {
-      logger.info("Show  get FCMToken  DeviceId : "+request.getDeviceId()+"  value : "+request.getFcmToken());
-      fakeTokenDB.put(request.getDeviceId(),request.getFcmToken());
-      return new ResponseEntity<>(new PushNotificationResponse(HttpStatus.OK.value(), "device add"), HttpStatus.OK);
-   }
+    @Autowired
+    public PushNotificationController(PushNotificationService pushNotificationService, TMPDB tmdb) {
+        this.pushNotificationService = pushNotificationService;
+        this.tmdb = tmdb;
+    }
 
-   //FIXME below search token send to one or all devices
-   @PostMapping("/notification/topic")
-   public ResponseEntity sendNotification(@RequestBody PushNotificationRequest request) {
-      pushNotificationService.sendPushNotificationWithoutData(request);
-      return new ResponseEntity<>(new PushNotificationResponse(HttpStatus.OK.value(), "Notification has been sent."), HttpStatus.OK);
-   }
 
-   @PostMapping("/notification/token")
-   public ResponseEntity sendTokenNotification(@RequestBody PushNotificationRequest request) {
-      pushNotificationService.sendPushNotificationToToken(request);
-      return new ResponseEntity<>(new PushNotificationResponse(HttpStatus.OK.value(), "Notification has been sent."), HttpStatus.OK);
-   }
+    @PostMapping("/registerToken")
+    public ResponseEntity registerFCMToken(@RequestBody FCMToken request) {
+        tmdb.addDevice(request.getDeviceId(), request.getFcmToken());
+        return new ResponseEntity<>(new PushNotificationResponse(HttpStatus.OK.value(), "device add"), HttpStatus.OK);
+    }
 
-   @PostMapping("/notification/data")
-   public ResponseEntity sendDataNotification(@RequestBody PushNotificationRequest request) {
-      pushNotificationService.sendPushNotification(request);
-      return new ResponseEntity<>(new PushNotificationResponse(HttpStatus.OK.value(), "Notification has been sent."), HttpStatus.OK);
-   }
+    @PostMapping("/getAllToken")
+    public ResponseEntity getAlltoken() {
+        return new ResponseEntity<>(new PushNotificationResponse(HttpStatus.OK.value(), tmdb.getAlltoken()), HttpStatus.OK);
+    }
+
+    @GetMapping("/getOneToken/{deviceId}")
+    public ResponseEntity getOneToken(@PathVariable("deviceId") String deviceId) {
+        return new ResponseEntity<>(new PushNotificationResponse(HttpStatus.OK.value(), tmdb.getOneToken(deviceId)), HttpStatus.OK);
+    }
+
+    @PostMapping("/notification/topic")
+    public ResponseEntity sendNotification(@RequestBody PushNotificationRequest request) {
+        pushNotificationService.sendPushNotificationWithoutData(request);
+        return new ResponseEntity<>(new PushNotificationResponse(HttpStatus.OK.value(), "Notification(topic) has been sent."), HttpStatus.OK);
+    }
+
+    @PostMapping("/notification/token")
+    public ResponseEntity sendTokenNotification(@RequestBody PushNotificationRequest request) {
+        pushNotificationService.sendPushNotificationToToken(request);
+        return new ResponseEntity<>(new PushNotificationResponse(HttpStatus.OK.value(), "Notification(token) has been sent."), HttpStatus.OK);
+    }
+
+    @PostMapping("/notification/data")
+    public ResponseEntity sendDataNotification(@RequestBody PushNotificationRequest request) {
+        pushNotificationService.sendPushNotification(request);
+        return new ResponseEntity<>(new PushNotificationResponse(HttpStatus.OK.value(), "Notification(data) has been sent."), HttpStatus.OK);
+    }
 }
